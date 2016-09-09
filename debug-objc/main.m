3ad632fbb51c719e24cecc416e3a60d59f9cb167
Man In The Middle <isaacselement@gmail.com>
2016-09-09 14:43:02 +0800

    initialize commit

---
+++ debug-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by maninthemiddle on 9/9/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
